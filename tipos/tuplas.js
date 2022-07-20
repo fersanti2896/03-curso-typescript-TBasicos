6bb800db5ff91d4f49be6dce107e01014054b5e6
Fernando Nicolás <fersanti2896@gmail.com>
2022-07-20 01:46:19 -0500

    Tuplas en tuplas.ts

---
+++ tipos/tuplas.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    const hero = ['Capitan Ginyu', 80000, false];
+    hero[0] = 'IronMan';
+    hero[1] = 1200;
+    console.log(hero);
+})();
---
+++ tipos/tuplas.ts
@@ -0,0 +1,7 @@
+(() => {
+    const hero: [string, number, boolean] = ['Capitan Ginyu', 80000, false];
+    hero[0] = 'IronMan';
+    hero[1] = 1200;
+
+    console.log(hero);
+})();
